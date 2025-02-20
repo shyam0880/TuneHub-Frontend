f1240665c1984ad77a82dc7378a7867044181166
shyam <Shyamlal9802@gmail.com>
2025-02-20 21:35:56 +0530

    Change Router feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./AutherContext/AuthContext"
 import Home from './Pages/Home';
 import Dashboard from './Pages/Dashboard';
@@ -10,17 +11,46 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 function App() {
+	const router = createBrowserRouter([
+	  {
+		path: "/",
+		element: <Home />,
+	  },
+	  {
+		path: "/Dashboard",
+		element: <ProtectedRoute />,
+		children: [
+			{	
+				path: "",
+				element: <Dashboard />,				
+				children: [
+					{
+						path: "Playlist",
+						element: <Playlist />,
+					},
+					{
+						path: "Payment",
+						element: <Payment />,
+					},
+					{
+						path: "createPlaylist",  
+						element: <CreatePlaylist />,
+					},
+					{
+						path: "likeSongs",
+						// element: <LikeSong />,
+					}
+				]
+			},
+			
+		],
+	},
+	]);
 	
 
   return (
-	  <Router>
-			<AuthProvider>
-				<Routes>
-				<Route path="/" element={<Home/>}/>
-				<Route path="/Dashboard" element={<Dashboard/>}/>
-				</Routes>
-			</AuthProvider>
-	</Router>
+	<AuthProvider>
+	</AuthProvider>
 )
 }
 
