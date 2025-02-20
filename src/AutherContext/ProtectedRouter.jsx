42113f8c463c5ea8fcbd99798cd4b9048761aeeb
shyam <Shyamlal9802@gmail.com>
2025-02-20 21:36:49 +0530

    Add ProtectedRouter

---
+++ src/AutherContext/ProtectedRouter.jsx
@@ -0,0 +1,15 @@
+import { Navigate, Outlet } from "react-router-dom";
+import AuthContext from '../AutherContext/AuthContext';
+import React ,{ useContext }from 'react';
+import Dashboard from '../Pages/Dashboard';
+
+const ProtectedRoute = () => {
+    const { contextUser } = useContext(AuthContext); 
+    if (contextUser === undefined) {
+        return <div>Loading...</div>; // ✅ Prevent redirecting too soon
+      }
+    
+      return contextUser ? <Outlet /> : <Navigate to="/" replace />;
+    };
+
+export default ProtectedRoute;
