ec0de690a8cdf40c120be59a3e6d72d2ec2f22aa
shyam <Shyamlal9802@gmail.com>
2025-03-15 19:08:23 +0530

    Update path and react loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./AutherContext/AuthContext"
-import Home from './Pages/Home';
-import Dashboard from './Pages/Dashboard';
+import ReactLoading from 'react-loading';
+const Home = lazy(() => import('./Pages/Home'));
+const Dashboard = lazy(() => import('./Pages/Dashboard'));
+const Payment = lazy(()=>import('./Payment/Payment'))
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Artist from './Component/Artist';
+const Playlist = lazy(() => import('./Component/Playlist'));
+const CreatePlaylist = lazy(() => import('./Component/CreatePlaylist'));
+const ProtectedRoute = lazy(() => import('./AutherContext/ProtectedRouter'));
 
 
 
@@ -17,7 +22,7 @@ function App() {
 		element: <Home />,
 	  },
 	  {
-		path: "/Dashboard",
+		path: "/dashboard",
 		element: <ProtectedRoute />,
 		children: [
 			{	
@@ -25,20 +30,20 @@ function App() {
 				element: <Dashboard />,				
 				children: [
 					{
-						path: "Playlist",
+						path: "playlist",
 						element: <Playlist />,
 					},
 					{
-						path: "Payment",
+						path: "payment",
 						element: <Payment />,
 					},
 					{
-						path: "createPlaylist",  
+						path: "create-playlist",  
 						element: <CreatePlaylist />,
 					},
 					{
-						path: "likeSongs",
-						// element: <LikeSong />,
+						path: "artist",
+						element: <Artist />,
 					}
 				]
 			},
@@ -50,6 +55,13 @@ function App() {
 
   return (
 	<AuthProvider>
+		<Suspense fallback={
+                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+                    <ReactLoading type="bubbles" color="#000" height={80} width={80} />
+                </div>
+        }>
+			<RouterProvider router={router} />
+		</Suspense>
 	</AuthProvider>
 )
 }
