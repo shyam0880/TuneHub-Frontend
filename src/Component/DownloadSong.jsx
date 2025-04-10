25a727e103733b3dcb0de838e1fa7f026f871442
shyam <Shyamlal9802@gmail.com>
2025-04-10 21:06:06 +0530

    Create Download song let you download song on local store

---
+++ src/Component/DownloadSong.jsx
@@ -0,0 +1,46 @@
+import React,{useState, useContext} from 'react';
+import "../CSS/DownloadSong.css";
+import DataContext from '../Context/DataContext';
+
+const DownloadSong = () => {
+    const {
+      downloadedSong,
+      currentSong,
+		  setCurrentSong,
+      handlePlaylistSong,
+		  isPlaying,
+    } = useContext(DataContext);
+
+    
+      return (
+        <div className="dwnsng">
+          <h2 className="heading">Downloaded Songs</h2>
+          <ul className="song-list">
+            {downloadedSong.length > 0 ? (
+              downloadedSong.map((song, index) => (
+                <li key={song.id} className="song-item">
+                  <span className="index">{index + 1}</span>
+                  <div className="img-play">
+                    <img src={song.imgLink} alt={song.name} />
+                  </div>
+                  <div className="song-info">
+                    <h5>
+                      {song.name} <br />
+                      <span className="subtitle">{song.artist}</span>
+                    </h5>
+                  </div>
+                  <div className="options">
+                  <i className={`bi ${currentSong.id === song.id && isPlaying ? "bi-pause-circle-fill" : "bi-play-circle-fill"}`} 
+											onClick={() => handlePlaylistSong(downloadedSong, index)}
+										></i>
+                  </div>
+                </li>
+              ))
+            ) : (
+              <li className="no-song">No matching songs found</li>
+            )}
+          </ul>
+        </div>
+      );
+    };
+export default DownloadSong
