c996a8decee6282806393282cc840c8550b6e724
shyam <Shyamlal9802@gmail.com>
2025-05-02 14:12:02 +0530

    Back to normal

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    historyApiFallback: true, // important for dev only
-  }
 })
